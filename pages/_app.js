f1b17d4ffb7fa58e11e9f53218d8c023a34d03d6
Anthony Morris <amorriscode@gmail.com>
2021-04-02 16:00:44 -0700

    Add basic SEO

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,29 @@
+import { DefaultSeo } from 'next-seo'
+
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <DefaultSeo
+        openGraph={{
+          type: 'website',
+          locale: 'en_IE',
+          url: 'https://getstx.me',
+          site_name: 'stx.me',
+          title: 'stx.me',
+          description: 'Hassle-free STX donations for any website',
+          images: [
+            {
+              url: 'https://getstx.me/og.png',
+              alt: 'Hassle-free STX donations for any website',
+            },
+          ],
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
