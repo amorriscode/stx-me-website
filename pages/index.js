3677a82f677b8f1a2c64e3516fcbbe2fb2519791
Anthony Morris <amorriscode@gmail.com>
2021-04-02 15:35:45 -0700

    Update head

--- pages/index.js
+++ pages/index.js
@@ -22,8 +22,31 @@ export default function Home() {
     <>
       <Head>
         <title>stx.me</title>
-        <link rel="icon" href="/favicon.ico" />
-        <script src="/bundle.js"></script>
+
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        ></link>
+
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        ></link>
+
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        ></link>
+
+        <link rel="manifest" href="/site.webmanifest"></link>
+
+        <script src="https://unpkg.com/stx-me/dist/stx.me.js"></script>
+
         <link
           rel="stylesheet"
           href="https://unpkg.com/stx-me/dist/stx.me.css"
